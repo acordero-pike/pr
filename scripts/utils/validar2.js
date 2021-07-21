a33adca41e55cb25558f5c3cb69f62d161fe949d
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-07-21 16:41:49 -0600

    Create validar2.js

---
+++ scripts/utils/validar2.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    prearray = JSON.parse( localStorage.getItem('Llave') ) || []  ;
+   
+    
+
+    if (prearray.length >0)
+{
+
+}else{
+    window.location.href="../pages/Home.html";
+}
+})
+   
