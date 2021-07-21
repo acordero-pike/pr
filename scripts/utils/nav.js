fcf6ee9055c59a66ef9c8ce56b2c1ef47fa916d2
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-07-21 13:07:11 -0600

    Update nav.js

--- scripts/utils/nav.js
+++ scripts/utils/nav.js
@@ -12,8 +12,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
    prearray.forEach( ar =>  {  nombre =ar.nombre})
 
-    document.querySelector("#logg").innerHTML=` <a class="nav-link"  style="color:white ;" href="../pages/updateUser.html">${nombre}</a>
-    <a class="nav-link"  style="color:white ;" id="logoff" >LogOff</a>`;
+    document.querySelector("#logg").innerHTML=`<nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Lerning2teach</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarsExample02">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="../pages/index.html"  >Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../pages/cursos.html">Cursos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">acerca de </a>
+            </li>
+          </ul>
+          <div class="navbar-nav" id="logg">
+        <a class="nav-link"  style="color:white ;" href="../pages/updateUser.html">${nombre}</a>
+    <a class="nav-link"  style="color:white ;" id="logoff" >LogOff</a>
+        </div>
+      </div>
+      </div>
+    </nav>
+  `;
     const logoff = document.querySelector("#logoff") ;
     logoff.addEventListener('click', () => {
 
@@ -32,8 +57,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 }else{
-    document.querySelector("#logg").innerHTML=` <a class="nav-link"  style="color:white ;" href="../pages/Login.html">Login </a>
-    <a class="nav-link"  style="color:white ;"   href="../pages/userRegister.html">singup </a>`;
+    document.querySelector("#nav-placeholder").innerHTML=`   <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Lerning2teach</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarsExample02">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="../pages/index.html"  >Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../pages/cursos.html">Cursos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">acerca de </a>
+            </li>
+          </ul>
+          <div class="navbar-nav" id="logg">
+       <a class="nav-link"  style="color:white ;" href="../pages/Login.html">Login </a>
+    <a class="nav-link"  style="color:white ;"   href="../pages/userRegister.html">singup </a>
+        </div>
+      </div>
+      </div>
+    </nav>
+    
+    `;
 }
 
 
