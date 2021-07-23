25a789b0e4dff4f6466e911ac7522c03ae55ca16
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-07-23 15:36:35 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -43,7 +43,7 @@ const openModalAdd = () => {
   currentCurso = null;
    const { nombre, descripcion, costo, idinstructor, duracion } =
     form.elements;
-console.log(curso);
+
   nombre.value = "";
   descripcion.value = "";
   costo.value = "";
